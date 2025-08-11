1ed5aaebd7e13a218c196dbbddc35b15a952906e
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-08-11 23:13:49 +0530

    Update -9

---
+++ Method_Details/src/StudentManagementSystem.java
@@ -0,0 +1,10 @@
+
+/**
+ * Main class demonstrating all method types in action
+ */
+public class StudentManagementSystem {
+	 public static void main(String[] args) {
+		 System.out.println("=== STUDENT MANAGEMENT SYSTEM ===");
+	     System.out.println("Demonstrating all types of Java methods\n");
+	 }
+}
