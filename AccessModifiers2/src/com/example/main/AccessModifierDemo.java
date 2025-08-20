921bdeec6db0196181a291b4c576c7fbf7b72afd
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-08-20 23:33:54 +0530

    Update -15

---
+++ AccessModifiers2/src/com/example/main/AccessModifierDemo.java
@@ -0,0 +1,11 @@
+package com.example.main;
+
+public class AccessModifierDemo {
+	public static void main(String[] args) {
+		System.out.println("Java Access Modifier Demonstration");
+        System.out.println("====================================");
+        
+        // Create instances and test access
+        BaseClass base = new BaseClass();
+	}
+}
--- AccessModifiers2/src/com/example/original/BaseClass.java
+++ AccessModifiers2/src/com/example/original/BaseClass.java
@@ -1,5 +1,43 @@
 package com.example.original;
 
 public class BaseClass {
-
+	public String publicField = "Public field";
+    protected String protectedField = "Protected field";
+    String defaultField = "Default field";  // package-private
+    private String privateField = "Private field";
+    
+    public void publicMethod() {
+        System.out.println("Public method called");
+    }
+    
+    protected void protectedMethod() {
+        System.out.println("Protected method called");
+    }
+    
+    void defaultMethod() {  // package-private
+        System.out.println("Default method called");
+    }
+    
+    private void privateMethod() {
+        System.out.println("Private method called");
+    }
+    
+    // Constructor demonstrating access within same class
+    public BaseClass() {
+        System.out.println("=== Same Class Access ===");
+        System.out.println("Public: " + this.publicField);
+        System.out.println("Protected: " + this.protectedField);
+        System.out.println("Default: " + this.defaultField);
+        System.out.println("Private: " + this.privateField);
+        
+        // All methods accessible within same class
+        this.publicMethod();
+        this.protectedMethod();
+        this.defaultMethod();
+        this.privateMethod();
+    }
+    
+    
+    
+    
 }
