1d155e2de3f5c3f8316d449a0785cf3da8307c93
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-08-20 21:33:26 +0530

    Update -3

--- AccessModifiers/src/com/example/package1/ProtectedParent.java
+++ AccessModifiers/src/com/example/package1/ProtectedParent.java
@@ -1,21 +1,53 @@
 package com.example.package1;
 
 public class ProtectedParent {
-	 protected String protectedField = "I am protected";
-	 protected static int protectedStaticField = 200;
+	protected String protectedField = "I am protected";
+	protected static int protectedStaticField = 200;
+
 	// Protected constructor
-	    protected ProtectedParent() {
-	        System.out.println("Protected constructor called");
-	    }
-	    
-	    // Protected method
-	    protected void protectedMethod() {
-	        System.out.println("This is a protected method");
-	    }
-	    
-	    // Protected static method
-	    protected static void protectedStaticMethod() {
-	        System.out.println("This is a protected static method");
-	    }
-	
+	protected ProtectedParent() {
+		System.out.println("Protected constructor called");
+	}
+
+	// Protected method
+	protected void protectedMethod() {
+		System.out.println("This is a protected method");
+	}
+
+	// Protected static method
+	protected static void protectedStaticMethod() {
+		System.out.println("This is a protected static method");
+	}
+
+	// Method to demonstrate access within same class'
+	public void accessProtectedMembers() {
+		System.out.println("Accessing from same class: " + protectedField);
+		protectedMethod();
+		protectedStaticMethod();
+	}
+}
+
+//out of ProtectedParent
+//Child class in same package
+class ProtectedChild extends ProtectedParent {
+	public void accessProtectedFromChild() {
+        // Can access protected members from parent
+        System.out.println("Accessing from child: " + protectedField);
+        protectedMethod();
+        protectedStaticMethod();
+    }
+}
+
+
+
+
+//Another class in same package (not inheritance)
+class ProtectedSamePackage {
+ public void accessProtected() {
+     ProtectedParent parent = new ProtectedParent();
+     // Can access protected members within same package
+     System.out.println("Same package access: " + parent.protectedField);
+     parent.protectedMethod();
+     ProtectedParent.protectedStaticMethod();
+ }
 }
