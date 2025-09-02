dabdd339e4966afffb9cd15a5c6e89f1cfe77575
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-09-02 22:14:08 +0530

    Update 7

--- Inheritance_All/src/Multiple_Inheritance/Dog.java
+++ Inheritance_All/src/Multiple_Inheritance/Dog.java
@@ -2,6 +2,4 @@ package Multiple_Inheritance;
 
 // ERROR: Java doesn't support multiple inheritance with classes
 //This line would cause compilation error
-class Dog extends Animal,Mammal {
-
-}
+//class Dog extends Animal,Mammal {}
