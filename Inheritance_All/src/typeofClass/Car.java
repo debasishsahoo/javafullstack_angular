1ea44fb62969c46d0884c1759af7dd731b9bf717
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-09-02 21:53:05 +0530

    Update 5

--- Inheritance_All/src/typeofClass/Car.java
+++ Inheritance_All/src/typeofClass/Car.java
@@ -5,4 +5,28 @@ class Car extends Vehicle { // THE SUBCLASS (Child)
 	// New, specific field only for Cars
 	private int doors;
 
+	// New, specific behavior only for Cars
+	public void honk() {
+		System.out.println("Car is honking");
+	}
+	// This method OVERRIDES the inherited start() & move() method for more specific
+	// behavior
+	
+	@Override
+	public void start() {
+		System.out.println("Car engine is starting");
+	}
+	
+	@Override
+    public void move() {
+        System.out.println("The car is driving on the road.");
+    }
+	
+	 // A method to display all properties, including the inherited 'brand'
+    public void displayDetails() {
+        System.out.println("Brand: " + brand); // Inherited from Vehicle
+        System.out.println("Doors: " + doors); // Specific to Car
+    }
+
+
 }
---
+++ Inheritance_All/src/typeofClass/Main.java
@@ -0,0 +1,8 @@
+package typeofClass;
+
+public class Main {
+	public static void main(String[] args) {
+		 Vehicle vehicle = new Vehicle();
+		
+	}
+}
