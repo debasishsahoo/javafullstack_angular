b6a24fd869e0c15bb545bb1f6aea447aa0159064
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-09-03 23:23:49 +0530

    Update -11

---
+++ Inheritance_All/src/constructorchaining/ConstructorChainingDemo.java
@@ -0,0 +1,38 @@
+package constructorchaining;
+class GrandParent {
+    public GrandParent() {
+        System.out.println("1. GrandParent constructor");
+    }
+}
+class Parent extends GrandParent {
+    public Parent() {
+        System.out.println("2. Parent constructor");
+    }
+}
+class Child extends Parent {
+    public Child() {
+        System.out.println("3. Child constructor");
+    }
+}
+public class ConstructorChainingDemo {
+	public static void main(String[] args) {
+		 // Creating object of Child
+		Child obj = new Child();
+	}
+}
+//Java automatically calls the constructor chain:
+//1. `GrandParent()` runs first (topmost).
+//2. Then `Parent()` runs.
+//3. Finally `Child()` runs.
+
+//**This happens because every constructor implicitly calls `super()` (unless you explicitly call a different one).**
+
+
+
+
+
+
+
+
+
+
