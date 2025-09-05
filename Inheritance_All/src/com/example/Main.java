819bddd0278e8e19ccc3f457d47b0791c80f3c3c
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-09-05 22:33:32 +0530

    Update -2

--- Inheritance_All/src/com/example/Main.java
+++ Inheritance_All/src/com/example/Main.java
@@ -12,10 +12,13 @@ public class Main {
 		
 		parent.defaultMethod(); 
 		child.defaultMethod();
+		child.test();
 		polyChild.defaultMethod();
-		
-		
-
+		polyChild.test();
 	}
 
 }
+
+//- **Default methods** are **package-private**, so they cannot be overridden in subclasses from another package.
+//- Polymorphic references **do not invoke the child’s “same-named” default method** if it is in a different package; they still call the parent’s method.
+//- Attempting to call the child’s default method from a class in another package **will cause a compilation error**.
