658eafb79f2c3b49813838ac0fcb7de38ce12478
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-09-21 13:34:25 +0530

    Update -5

--- Abstract_Class/src/project_1/Dog.java
+++ Abstract_Class/src/project_1/Dog.java
@@ -1,63 +1,35 @@
 package project_1;
 
 class Dog extends Animal {
-private boolean canFly;
-    
-    public Bird(String name, int age, boolean canFly) {
-        super(name, age);
-        this.canFly = canFly;
-    }
-    
-    @Override
-    public void makeSound() {
-        System.out.println(name + " chirps: Tweet! Tweet!");
-    }
-    
-    @Override
-    public void move() {
-        if (canFly) {
-            System.out.println(name + " flies through the air");
-        } else {
-            System.out.println(name + " hops on the ground");
-        }
-    }
-    
-    @Override
-    public void eat(String food) {
-        System.out.println(name + " pecks at " + food);
-    }
-    
-    // Bird-specific method
-    public void buildNest() {
-        System.out.println(name + " builds a cozy nest");
-    }private boolean canFly;
-    
-    public Bird(String name, int age, boolean canFly) {
-        super(name, age);
-        this.canFly = canFly;
-    }
-    
-    @Override
-    public void makeSound() {
-        System.out.println(name + " chirps: Tweet! Tweet!");
-    }
-    
-    @Override
-    public void move() {
-        if (canFly) {
-            System.out.println(name + " flies through the air");
-        } else {
-            System.out.println(name + " hops on the ground");
-        }
-    }
-    
-    @Override
-    public void eat(String food) {
-        System.out.println(name + " pecks at " + food);
-    }
-    
-    // Bird-specific method
-    public void buildNest() {
-        System.out.println(name + " builds a cozy nest");
-    }
+	 private String breed;
+	    
+	    public Dog(String name, int age, String breed) {
+	        super(name, age);
+	        this.breed = breed;
+	    }
+	    
+	    @Override
+	    public void makeSound() {
+	        System.out.println(name + " barks: Woof! Woof!");
+	    }
+	    
+	    @Override
+	    public void move() {
+	        System.out.println(name + " runs on four legs");
+	    }
+	    
+	    @Override
+	    public void eat(String food) {
+	        System.out.println(name + " eagerly eats " + food);
+	    }
+	    
+	    // Dog-specific method
+	    public void wagTail() {
+	        System.out.println(name + " wags tail excitedly!");
+	    }
+	    
+	    @Override
+	    public String getInfo() {
+	        return super.getInfo() + ", Breed: " + breed;
+	    }
 }
--- Abstract_Class/src/project_1/Fish.java
+++ Abstract_Class/src/project_1/Fish.java
@@ -1,5 +1,26 @@
 package project_1;
 
 class Fish extends Animal{
+	 private String waterType;
+	    
+	    public Fish(String name, int age, String waterType) {
+	        super(name, age);
+	        this.waterType = waterType;
+	    }
+	    
+	    @Override
+	    public void makeSound() {
+	        System.out.println(name + " makes bubbles: Blub blub...");
+	    }
+	    
+	    @Override
+	    public void move() {
+	        System.out.println(name + " swims gracefully in " + waterType + " water");
+	    }
+	    
+	    @Override
+	    public void eat(String food) {
+	        System.out.println(name + " gulps down " + food);
+	    }
 
 }
