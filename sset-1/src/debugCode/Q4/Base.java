336177618640c9f762c273f0c582a4d9aa8dbd68
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-10-04 21:26:24 +0530

    Update-11 -Set-1

---
+++ sset-1/src/debugCode/Q4/Base.java
@@ -0,0 +1,11 @@
+package debugCode.Q4;
+
+public class Base {
+	public static void staticMethod() {
+        System.out.println("Base static method");
+    }
+
+    public void instanceMethod() {
+        System.out.println("Base instance method");
+    }
+}
