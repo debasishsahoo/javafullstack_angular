a9e8eb703a58e0fac9dca8326db3e14b71ea7055
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-10-04 21:17:24 +0530

    Update-10 -Set-1

--- sset-1/src/debugCode/Q3/BankTransferPayment.java
+++ sset-1/src/debugCode/Q3/BankTransferPayment.java
@@ -1,7 +1,7 @@
 package debugCode.Q3;
 
 public class BankTransferPayment implements PaymentMethod, Refundable, FeeCalculable{
-	 private String accountNumber;
+	    private String accountNumber;
 	    private String routingNumber;
 	    private static final double BANK_FEE = 2.50; // Fixed bank fee
 }
