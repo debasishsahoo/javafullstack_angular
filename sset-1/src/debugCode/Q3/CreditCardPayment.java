954bebfc381a33dda59911cdc202dac2a400a6f3
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-10-04 20:38:01 +0530

    Update-5 -Set-1

--- sset-1/src/debugCode/Q3/CreditCardPayment.java
+++ sset-1/src/debugCode/Q3/CreditCardPayment.java
@@ -26,19 +26,36 @@ public class CreditCardPayment implements PaymentMethod, Authenticable, Refundab
 		return cardNumber != null && !cardNumber.isEmpty() && expiryDate != null && !expiryDate.isEmpty() && cvv != null
 				&& cvv.length() == 3;
 	}
-	
-	 @Override
-	    public String getPaymentDetails() {
-	        return "Credit Card ending in " + cardNumber.substring(cardNumber.length() - 4);
-	    }
-	 @Override
-	    public String getPaymentType() {
-	        return "Credit Card";
-	    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Override
+	public String getPaymentDetails() {
+		return "Credit Card ending in " + cardNumber.substring(cardNumber.length() - 4);
+	}
 
+	@Override
+	public String getPaymentType() {
+		return "Credit Card";
+	}
+
+	@Override
+	public boolean authenticate(String credential) {
+		return this.authToken != null && this.authToken.equals(credential);
+	}
+
+	@Override
+	public void setAuthToken(String token) {
+		this.authToken = token;
+	}
+
+	@Override
+	public boolean processRefund(double amount, String transactionId) {
+		System.out.println("Processing refund of $" + amount + " for transaction: " + transactionId);
+		return true;
+	}
+
+	@Override
+	public String getRefundPolicy() {
+		return "Refunds processed within 7-10 business days";
 	}
 
 }
