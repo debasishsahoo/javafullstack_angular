7f4011a3d2fd0fd7e404669cfded19143aaef78a
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-10-28 20:57:17 +0530

    Hashset-9

--- Collectionframework/src/com/java/collection/set/navigableset/NavigableSetExample.java
+++ Collectionframework/src/com/java/collection/set/navigableset/NavigableSetExample.java
@@ -14,10 +14,28 @@ public class NavigableSetExample {
 
         System.out.println("NavigableSet: " + numbers);
 
-        System.out.println("Lower(30): " + numbers.lower(30));   // < 30  //Returns the greatest element less than e.
-        System.out.println("Floor(30): " + numbers.floor(30));   // <= 30 //Returns the greatest element ≤ e.
-        System.out.println("Higher(30): " + numbers.higher(30)); // > 30  //Returns the smallest element greater than e.
-        System.out.println("Ceiling(30): " + numbers.ceiling(30)); // >= 30 //Returns the smallest element ≥ e.
+        System.out.println("Lower(30): " + numbers.lower(30));       // < 30  //Returns the greatest element less than e.
+        System.out.println("Floor(30): " + numbers.floor(30));       // <= 30 //Returns the greatest element ≤ e.
+        System.out.println("Higher(30): " + numbers.higher(30));     // > 30  //Returns the smallest element greater than e.
+        System.out.println("Ceiling(30): " + numbers.ceiling(30));   // >= 30 //Returns the smallest element ≥ e.
+        
+        
+        System.out.println("headSet(30): " + numbers.headSet(30));         // Elements less than 30
+        System.out.println("tailSet(30): " + numbers.tailSet(30));         // Elements greater than or equal to 30
+        System.out.println("subSet(20, 40): " + numbers.subSet(20, 40));   // Elements between 20 and 40 (exclusive of 40)
+        
+        NavigableSet<Integer> reverseSet = numbers.descendingSet();
+        System.out.println("Descending Set: " + reverseSet);
+        
+        
+        System.out.println("pollFirst(): " + numbers.pollFirst());
+        System.out.println("pollLast(): " + numbers.pollLast());
+        System.out.println("Set after poll operations: " + numbers);
+        
+        
+        
+        
+        
     }
 }
 
